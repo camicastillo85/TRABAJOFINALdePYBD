41396572fa5b8814e6d05758303f72856fb79c67
Daniel V <Daniel V@DESKTOP-7NGJ1VH>
2024-11-17 11:17:01 -0300

    IndexCompre Prueba de avance Vargas Daniel

--- src/main/java/ar/edu/ies6/controller/CompraController.java
+++ src/main/java/ar/edu/ies6/controller/CompraController.java
@@ -1,5 +1,14 @@
 package ar.edu.ies6.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CompraController {
 
+	@GetMapping("/compras")
+	public String getIndex() {
+		System.out.println("ingreso por aqui IndexCompras");
+		return "indexCompra";
+	}
 }
--- src/main/java/ar/edu/ies6/model/Compra.java
+++ src/main/java/ar/edu/ies6/model/Compra.java
@@ -1,81 +1,113 @@
 package ar.edu.ies6.model;
 
 import java.time.LocalDate;
-import java.util.List;
+//import java.util.List;
 
+import org.springframework.stereotype.Component;
+
+//import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Component
+@Entity
 public class Compra {
-
+	@Id
+	private String idCompra;
 	private String id; 
-	private Cliente cliente;
-	private List<Producto> productos;
-	private LocalDate fecha; 
-	private Double total;
 	
-	
-	public Compra() {
+	//@ManyToOne 
+		//private Cliente cliente;
+		//@ManyToOne 
+		//private List<Producto> productos;
+		//@Column
+		private LocalDate fecha; 
+		//@Column
+		private Double total;
 		
-	}
+		
+		public Compra() {
+			
+		}
 
 
-	public Compra(String id, Cliente cliente, List<Producto> productos, LocalDate fecha, Double total) {
-		super();
-		this.id = id;
-		this.cliente = cliente;
-		this.productos = productos;
-		this.fecha = fecha;
-		this.total = total;
-	}
+		/**public Compra(String id,String idCompra, Cliente cliente, List<Producto> productos, LocalDate fecha, Double total) {
+			super();
+			this.id = id;
+			this.idCompra=idCompra;
+			this.cliente = cliente;
+			this.productos = productos;
+			this.fecha = fecha;
+			this.total = total;
+		}*/
+		//templates indexCompra
+		public Compra(String id,String idCompra, LocalDate fecha, Double total) {
+			super();
+			this.id = id;
+			this.idCompra=idCompra;
+			this.fecha = fecha;
+			this.total = total;
+		}
 
 
-	public String getId() {
-		return id;
-	}
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getIdCompra() {
+			return idCompra;
+		}
 
 
-	public void setId(String id) {
-		this.id = id;
-	}
+		public void setIdCompra(String idCompra) {
+			this.idCompra = idCompra;
+		}
 
 
-	public Cliente getCliente() {
-		return cliente;
-	}
+		/**
+		public Cliente getCliente() {
+			return cliente;
+		}
 
 
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
+		public void setCliente(Cliente cliente) {
+			this.cliente = cliente;
+		}
 
 
-	public List<Producto> getProductos() {
-		return productos;
-	}
+		public List<Producto> getProductos() {
+			return productos;
+		}
 
 
-	public void setProductos(List<Producto> productos) {
-		this.productos = productos;
-	}
+		public void setProductos(List<Producto> productos) {
+			this.productos = productos;
+		}*/
 
 
-	public LocalDate getFecha() {
-		return fecha;
-	}
+		public LocalDate getFecha() {
+			return fecha;
+		}
 
 
-	public void setFecha(LocalDate fecha) {
-		this.fecha = fecha;
-	}
+		public void setFecha(LocalDate fecha) {
+			this.fecha = fecha;
+		}
 
 
-	public Double getTotal() {
-		return total;
-	}
+		public Double getTotal() {
+			return total;
+		}
 
 
-	public void setTotal(Double total) {
-		this.total = total;
-	}
-	
-	
-	
+		public void setTotal(Double total) {
+			this.total = total;
+		}
+		
+		
 }
