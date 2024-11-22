b33f3117b38ec13d9baaae7d05a6d6daa09e82c6
MI-PC <MI-PC@DESKTOP-OEM8JLM>
2024-11-22 13:19:49 -0300

    subiendo cliente controller aunque no carga la pagina sale como un buscador comun...Mariana Guerrero

--- src/main/java/ar/edu/ies6/controller/ClienteController.java
+++ src/main/java/ar/edu/ies6/controller/ClienteController.java
@@ -1,5 +1,49 @@
 package ar.edu.ies6.controller;
 
-public class ClienteController {
+import org.springframework.stereotype.Controller;
 
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.ies6.model.Cliente;
+import ar.edu.ies6.service.IClienteService;
+
+@Controller
+public class ClienteController {
+	//gestiona el acceso ala vista
+	@Autowired
+	Cliente unCliente;
+
+@Qualifier("servicioClienteBD")
+     IClienteService clienteService;
+	
+//creado del cliente
+@GetMapping("/cliente")
+public ModelAndView getIndexWithCliente () {
+	//transporte hacia la vista
+	
+	ModelAndView transportador = new ModelAndView("cliente");
+	transportador.addObject("cliente", "unCliente");
+	
+	return transportador;
+}
+
+@PostMapping("/guardarCliente")
+public ModelAndView guardarCliente(Cliente cliente) {
+	
+	//ClienteServiceImp clienteService = new ClienteServiceImp();
+	clienteService.guardarCliente(cliente);
+	
+	ModelAndView transportador = new ModelAndView("listaCliente");
+	transportador.addObject("listadoCliente", clienteService.listarTodosCliente());
+	
+	return transportador;
+}
 }
